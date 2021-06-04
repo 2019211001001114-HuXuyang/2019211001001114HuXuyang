9f2dc6fcf0a815734c5f783e782fa21b061a0212
2219263440@qq.com <12751314hxy>
2021-06-04 12:29:54 +0800

    lab1 Commit

---
+++ src/main/java/com/HuXuyang/controller/LabServlet.java
@@ -0,0 +1,17 @@
+package com.HuXuyang.controller;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/inLab")
+public class LabServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("lab1/assignment2/login.jsp").forward(request,response);
+    }
+}
